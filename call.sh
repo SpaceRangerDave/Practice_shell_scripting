e7dd7b10d9414fbc92b467f6d95429ee85275935
SpaceRangerDave <d.kakhayang@alustudent.com>
2026-09-22 12:00:24 +0200

    Updates call.sh Script

--- call.sh
+++ call.sh
@@ -4,9 +4,9 @@ chmod u+x call.sh
 x=Dave
 echo $x
 
-mkdir -p Cohorts
-mkdir -p Courses
+mkdir Cohorts
+mkdir  Courses
 
 git add .
-git commit -m'Our First Script Practice'
+git commit -m'Updates call.sh Script'
 git push
